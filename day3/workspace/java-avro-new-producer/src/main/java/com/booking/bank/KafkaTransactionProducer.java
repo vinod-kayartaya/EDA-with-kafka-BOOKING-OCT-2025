b79227e8597abd0f7b0a7a65c214fea5626692f0
Vinod Kumar Kayartaya <vinod@vinod.co>
2025-10-29 15:27:30 +0530

    end of day 3 code

---
+++ day3/workspace/java-avro-new-producer/src/main/java/com/booking/bank/KafkaTransactionProducer.java
@@ -0,0 +1,36 @@
+package com.booking.bank;
+
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.time.Instant;
+import java.util.Properties;
+
+public class KafkaTransactionProducer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
+        props.put("schema.registry.url", "http://localhost:8081");
+
+        String topic = "transactions";
+
+        KafkaProducer<String, TransactionEvent> producer = new KafkaProducer<>(props);
+        TransactionEvent te = TransactionEvent.newBuilder()
+                .setAmount(50000)
+                .setTxId(123)
+                .setTxType("cheque")
+                .setTimestamp(Instant.now())
+                .build();
+
+        ProducerRecord<String, TransactionEvent> record =
+                new ProducerRecord<>(topic, "tx-"+te.getTxId(), te);
+        producer.send(record);
+        producer.flush();
+        producer.close();
+    }
+}
---
+++ day3/workspace/java-kafka-stream-demo/src/main/java/com/booking/kafka/streams/AlertLargeTransactions.java
@@ -0,0 +1,47 @@
+package com.booking.kafka.streams;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Properties;
+
+public class AlertLargeTransactions {
+
+    public static void main(String[] args) {
+
+        StreamsBuilder builder = new StreamsBuilder();
+        String sourceTopic = "transactions";
+        String targetTopic = "largeTransactions";
+
+        // represents the source event topic
+        KStream<String, String> transactions = builder.stream(sourceTopic);
+
+        // filter transactions with amount >= 50000
+        KStream<String, String> largeTransactions =
+                transactions.filter((k, v) -> {
+                            // value --> id=222;type=cash;amount=34000
+                            var data = v.split(";");
+                            var amountData = data[2].split("=");
+                            var amount = Double.parseDouble(amountData[1]);
+                            return amount >= 50000;
+                        })
+                        .peek((k, v) -> {
+                            System.out.printf("key = %s, value = %s\n", k, v);
+                        });
+
+        largeTransactions.to(targetTopic);
+
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "bank-transactions");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+}
